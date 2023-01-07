5e6131aa7f54b5361b137b6e73e7bd0f69c0dba8
thedexter45 <96930258+thedexter45@users.noreply.github.com>
2023-01-07 18:51:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -10,10 +10,10 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚅𝙴𝚁𝚂𝙸𝙾𝙽: 𝙹𝙰𝚂𝙼𝙸𝙽𝙴-𝙱𝙾𝚃 𝚟3.0.0"""
 
     SOURCE_TXT = """<b>NOTE:</b>
-- 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁 👉 :<a href=https://github.com/thedexter45/Jasmine>𝐉𝐀𝐒𝐌𝐈𝐍𝐄-𝐁𝐎𝐓</a>
+- 𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴 𝙲𝙻𝙸𝙲𝙺 𝙷𝙴𝚁 👉 : <a href=https://github.com/thedexter45/Jasmine>𝐉𝐀𝐒𝐌𝐈𝐍𝐄-𝐁𝐎𝐓</a>
 
 <b>DEVS:</b>
-- 𝙳𝚎𝚟 1<a href=https://t.me/dexte_r_46>𝐃𝐄𝐗𝐓𝐄𝐑</a>"""
+- 𝙳𝚎𝚟 1 <a href=https://t.me/dexte_r_46>𝐃𝐄𝐗𝐓𝐄𝐑</a>"""
 
     FILE_TXT = """➤ 𝐇𝐞𝐥𝐩: 𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞 𝐌𝐨𝐝𝐮𝐥𝐞../
 
